7ea1c7f59b537552f6c2cf564459f84e952f6cd7
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-25 10:51:56 -0400

    added get one category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,6 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// GET /api/categories
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
@@ -22,22 +23,55 @@ router.get('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   })
-  // be sure to include its associated Products
 });
 
+// GET /api/categories/1
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+// POST /api/categories
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
+// PUT /api/categories/1
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
+// DELETE /api/categories/1
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
