8a9efc368e85c88ceff60271e5ef1764d82a6b36
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-25 09:58:22 -0400

    added model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongsToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+})
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongsToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
